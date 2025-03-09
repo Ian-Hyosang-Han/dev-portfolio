e60bd1b0daa2858855509ec64898684dfb459cb7
HHan0330 <kkoma330@gmail.com>
2025-03-09 15:03:56 -0700

    have integrated the about section, works section, and skills section, and have meticulously designed the layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,25 +36,25 @@ function Home() {
 
     return (
         <div className="wrapper">
-            <section className="intro">
+            <section className="intro-section">
                 <div className="role">
                     <p className="front-end">Front-End</p>
                     <p>Developer</p>
                 </div>
                 <div className="icons">
-                    <a href="https://www.linkedin.com/in/ian-hyosang-han" 
-                       target="_blank" 
-                       rel="noopener noreferrer">
-                        <img src={linkedInIcon} 
-                             alt="LinkedIn Icon" 
-                             className="icon-1" />
+                    <a href="https://www.linkedin.com/in/ian-hyosang-han"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <img src={linkedInIcon}
+                            alt="LinkedIn Icon"
+                            className="icon-1" />
                     </a>
-                    <a href="https://github.com/Ian-Hyosang-Han" 
-                       target="_blank" 
-                       rel="noopener noreferrer">
-                        <img src={githubIcon} 
-                             alt="GitHub Icon" 
-                             className="icon-2" />
+                    <a href="https://github.com/Ian-Hyosang-Han"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <img src={githubIcon}
+                            alt="GitHub Icon"
+                            className="icon-2" />
                     </a>
                 </div>
                 <div className="greeting">
@@ -66,6 +66,73 @@ function Home() {
                     ))}
                 </div>
             </section>
+            <section id='home-about' className='about-section'>
+                <div className='angle-top'></div>
+                <div className="section-inner">
+                    <div className="inner-wide">
+                        <h2>about</h2>
+                        <p><strong>Driven by limitless ideas and imagination.</strong> I will be a developer who constantly strives  for endless development and problem-solving. With the skills I have learned, I will maintain an attitude of continuous growth by staying updated with the latest trends ans tools</p>
+                        <p><strong>If you're not making mistakes, you're not doing anything.</strong> I believe that challenging moments are inevitable for everyone. However, the ultimate  purpose of these challenges is to learn and <strong>grow form the experience.</strong> By approaching even the smallest problems with patience and resilience, fostering communication with those around me, and turning each experience into an opportunity for personal growth, I strive to maintain a healthy work-life balance while continually progressing in my journey.</p>
+                    </div>
+                </div>
+                <div className='angle-bottom'></div>
+            </section>
+
+            <section id='home-works' className='works-section'>
+                <div className="work work01">
+                    <div className="work-cover"></div>
+                    <div className="work-text">
+                        <h2>astro escape</h2>
+                        <p>javascript-game</p>
+                    </div>
+                </div>
+
+                <div className="work work02">
+                    <div className="work-cover"></div>
+                    <div className="work-text">
+                        <h2>movella</h2>
+                        <p>movie database</p>
+                    </div>
+                </div>
+
+                <div className="work work03">
+                    <div className="work-cover"></div>
+                    <div className="work-text">
+                        <h2>capstone project</h2>
+                        <p>capstone project</p>
+                    </div>
+                </div>
+
+                <div className="work work04">
+                    <div className="work-cover"></div>
+                    <div className="work-text">
+                        <h2>design</h2>
+                        <p>figma/adobe xd</p>
+                    </div>
+                </div>
+
+                <div className="work-inner">
+                    <div className="work-inner-wide">
+                        <h2>works</h2>
+                    </div>
+                </div>
+            </section>
+
+            <section id='home-skills' className='skill-section'>
+                <div className='angle-top'></div>
+                <div className="section-inner">
+                    <div className="inner-wide">
+                        <h2>skills</h2>
+                        <h3>development</h3>
+                        <p>HTML / CSS / JAVASCRIPT / SASS / REACT / PHP / 
+                        Shopify / woocommerce / wordPress</p>
+                        <h3>design tools</h3>
+                        <p>Figma / Adobe XD</p>
+                    </div>
+                </div>
+                <div className='angle-bottom'></div>
+            </section>
+
         </div>
     );
 }
