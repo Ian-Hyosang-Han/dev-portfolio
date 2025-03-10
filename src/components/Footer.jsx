a477fa258cbf2e82e67feb3b720ee16949b8578b
HHan0330 <kkoma330@gmail.com>
2025-03-10 10:16:47 -0700

    add the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+
+    // Get dates function
+    function getYear() {
+        const d = new Date();
+        return d.getFullYear();
+    }
+
+    return (
+        <footer className="p-4 text-white text-right mt-5">
+            <p>&copy; {getYear()} Ian's Portfolio</p>
+            <p>unspecialty</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Home from "../pages/Home";
 
 function AppRouter() {
 
     return (
         <BrowserRouter>
-        <Header />
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 {/* <Route path="/about" element={<About />} />
@@ -15,6 +16,7 @@ function AppRouter() {
                 <Route path="/capstone" element={<About />} />
                 <Route path="/designtools" element={<About />} /> */}
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 
