49e10b1ce664f85d1d945eca230063ad239fc5b9
HHan0330 <kkoma330@gmail.com>
2025-03-16 21:46:05 -0700

    add the media query for home page and about page. also add the text-image background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -85,30 +85,17 @@ function Home() {
             <section id='home-about' className='about-section'>
                 <div className='angle-top'></div>
                 <div className="section-inner">
+                        <h2>about</h2>
                     <div className="inner-wide">
                         <div ref={gifRef} className="gif-container"></div>
-                        <h2>about</h2>
-                        <p><strong data-gif="src/media/gif-1.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Driven by limitless ideas and imagination.</strong> I will be a developer who constantly strives  for endless development and problem-solving. With the skills I have learned, I will maintain an attitude of continuous growth by staying updated with the latest trends ans tools</p>
-                        <p><strong data-gif="src/media/gif-2.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>If you're not making mistakes, you're not doing anything.</strong> I believe that challenging moments are inevitable for everyone. However, the ultimate  purpose of these challenges is to learn and <strong data-gif="src/media/gif-3.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>grow from the experience.</strong> By approaching even the smallest problems with patience and resilience, fostering communication with those around me, and turning each experience into an opportunity for personal growth, I strive to maintain a healthy work-life balance while continually progressing in my journey.</p>
+                        <p><strong className="gif" data-gif="src/media/gif-1.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Driven by limitless ideas and imagination.</strong> I will be a developer who constantly strives  for endless development and problem-solving. With the skills I have learned, I will maintain an attitude of continuous growth by staying updated with the latest trends ans tools</p>
+                        <p><strong className="gif" data-gif="src/media/gif-2.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>If you're not making mistakes, you're not doing anything.</strong> I believe that challenging moments are inevitable for everyone. However, the ultimate  purpose of these challenges is to learn and <strong className="gif" data-gif="src/media/gif-3.gif" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>grow from the experience.</strong> By approaching even the smallest problems with patience and resilience, fostering communication with those around me, and turning each experience into an opportunity for personal growth, I strive to maintain a healthy work-life balance while continually progressing in my journey.</p>
                     </div>
                 </div>
                 <div className='angle-bottom'></div>
             </section>
 
-            <section id='home-skills' className='skill-section'>
-                <div className='angle-top'></div>
-                <div className="section-inner">
-                    <div className="inner-wide">
-                        <h2>skills</h2>
-                        <h3>development</h3>
-                        <p>HTML / CSS / JAVASCRIPT / SASS / REACT / PHP /
-                            Shopify / woocommerce / wordPress</p>
-                        <h3>design tools</h3>
-                        <p>Figma / Adobe XD</p>
-                    </div>
-                </div>
-                <div className='angle-bottom'></div>
-            </section>
+
 
             <section id='home-works' className='works-section'>
                 <div className="work work01">
@@ -148,6 +135,21 @@ function Home() {
                         <h2>works</h2>
                     </div>
                 </div>
+            </section>
+
+            <section id='home-skills' className='skill-section'>
+                <div className='angle-top'></div>
+                <div className="skill-section-inner">
+                    <div className="skill-inner-wide">
+                        <h2>skills</h2>
+                        <h3>development</h3>
+                        <p>HTML / CSS / JAVASCRIPT / SASS / REACT / TAILWIND CSS / PHP /
+                            SHOPIFY / WOOCOMMERCE / WORDPRESS</p>
+                        <h3>design tools</h3>
+                        <p>Figma / Adobe XD</p>
+                    </div>
+                </div>
+                <div className='angle-bottom'></div>
             </section>
         </div>
     );
